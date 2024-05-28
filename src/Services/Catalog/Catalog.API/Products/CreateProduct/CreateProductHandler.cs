295072856efdf264bfa5e9c36a47abeb33a2997f
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2024-05-28 13:33:19 +0100

    Commit staged

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -10,12 +10,23 @@
     //Represents the response object
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required"); 
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required"); 
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0"); 
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session,ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //Business logic to create a new product
-
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
             //create Product entity from command object
             var product = new Product
             {
