a49e04b193799d99078bfa99cdbf9753e34e6f72
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2024-06-06 04:31:18 +0100

    Added Domain Driven Design with Clean Architecture folder structure

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,14 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services
-//    .AddApplicationServices(builder.Configuration)
-//    .AddInfrastructureServices(builder.Configuration)
-//    .AddApiServices(builder.Configuration);
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
